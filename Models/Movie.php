30aa90dcdf0b33d02bf27c77bb173cc774657ac4
Dan848 <daniele.ceravolo15@gmail.com>
2023-05-16 17:17:23 +0200

    populate db

--- Models/Movie.php
+++ Models/Movie.php
@@ -1,9 +1,10 @@
 <?php
 include __DIR__. '/Genre.php';
-class Movies
+class Movie
 {
     public int $id;
     public string $title;
+    public string $cover_image;
     public int $relase_year;
 
     public Genre $genre;
@@ -12,10 +13,11 @@ class Movies
 
     public int $rating;
 
-    public function __construct(int $id, string $title, int $relase_year, Genre $genre, string $director, int $rating)
+    public function __construct(int $id, string $title, string $cover_image, int $relase_year, Genre $genre, string $director, int $rating)
     {
         $this->id = $id;
         $this->title = $title;
+        $this->cover_image = $cover_image;
         $this->relase_year = $relase_year;
         $this->genre = $genre;
         $this->director = $director;
---
+++ db.php
@@ -0,0 +1,33 @@
+<?php
+include __DIR__ . '/Models/Movies.php';
+
+$movies = [
+    new Movie(
+        "1",
+        "Avatar - La via dell'acqua",
+        "https://upload.wikimedia.org/wikipedia/it/thumb/e/e1/Avatar_-_La_via_dell%27acqua.png/1920px-Avatar_-_La_via_dell%27acqua.png",
+        2022,
+        new Genre(["fantascienza", "azione", "avventura"], false),
+        "James Cameron",
+        8
+    ),
+    new Movie(
+        "2",
+        "Joker",
+        "https://upload.wikimedia.org/wikipedia/it/f/f2/Joker_%28film_2019%29.png",
+        2019,
+        new Genre(["drammatico", "poliziesco", "thriller", "noir"], true),
+        "Todd Phillips",
+        9.4
+    ),
+    new Movie(
+        "3",
+        "Lupin III - Il castello di Cagliostro",
+        "https://upload.wikimedia.org/wikipedia/it/1/1b/Logo_del_Castello_di_Cagliostro.png",
+        1979,
+        new Genre(["drammatico", "poliziesco", "thriller", "noir"], false),
+        "Hayao Miyazaki",
+        8.7
+    ),
+    
+];
